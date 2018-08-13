81c195fa01e62f7ec1692f3e9af631c5a4cb1ada
riyaranjan336 <42323169+riyaranjan336@users.noreply.github.com>
2018-08-14 02:28:37 +0530

    Update p3.java

--- p3.java
+++ p3.java
@@ -1,13 +1,17 @@
 public class p3 {
 
 	public static void main(String[] args) {
-	Double n=13195.0;
+	Double n=600851475143.0;
 	for(int i=2;i<=n;i++)
 	{
 		while(n%i==0)
 		{
-			System.out.println(i);
 			n=n/i;
+			while(n==1)
+			{
+				System.out.println(i);
+				break;
+			}
 		}
 	}
 
